3f9c88646bea9d26c52374787451a1a1f184cdb3
Tomas Alves de Souza <tomas.alves08@gmail.com>
2022-06-25 09:35:41 +1200

    Add CSS styles on the script when player guess the right number.

--- script.js
+++ script.js
@@ -17,7 +17,6 @@
 
 const secretNumber = Math.floor(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -25,23 +24,30 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (score > 1) {
     // There is no input!
+
     if (!guess) {
       document.querySelector(".message").textContent = "no number ⛔️";
+
       // When player gets the right number!
     } else if (guess === secretNumber) {
       document.querySelector(".message").textContent = "Correct Number! 🎉";
-      document.querySelector("body").style(ba);
+      document.querySelector("body").style.backgroundColor = "#60b347";
+      document.querySelector(".number").style.width = "30rem";
+      document.querySelector(".number").textContent = secretNumber;
+
       // When too high!!
     } else if (guess > secretNumber) {
       document.querySelector(".message").textContent = "Too high! 📈";
       score--;
       document.querySelector(".score").textContent = score;
+
       // When too low!
     } else if (guess < secretNumber) {
       document.querySelector(".message").textContent = "Too low! 📉";
       score--;
       document.querySelector(".score").textContent = score;
     }
+
     // When loses the game!!
   } else {
     document.querySelector(".message").textContent = "You lost the game! 💥";
